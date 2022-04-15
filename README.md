7fecd54f26d92ff7c9862effb59c6221e86d9a63
Ewelina Szmielew <ewelina@szmielew.pl>
2022-04-15 23:07:01 +0200

    Add app and project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # doggo_school
+
+This app will help you train effectively with your dog using different training plans offered by proffesional dog trainers.
---
+++ teaching_platform/doggo_school/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DoggoSchoolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'doggo_school'
